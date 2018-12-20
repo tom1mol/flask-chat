d997ecf743f0ad5521e0798191deb54f68dd7e3c
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-12-20 19:52:01 +0000

    implement basic views and routing

--- run.py
+++ run.py
@@ -5,7 +5,19 @@ app = Flask(__name__)       #initialise new flask app
 
 @app.route('/')     #root decorator for index page
 def index():            #define function that'll be bound to our decorator
-    return "<h1>Hello There!</h1>"
+
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE"
+    
+    
+@app.route('/<username>')       #this gets treated as a variable(username)
+def user(username):             #a function that will bind to the route decorator. argument of username
+    return "Hi " + username          #return to the user hi + username
+    
+    
+@app.route('/<username>/<message>')
+def send_message(username, message):     #function that is binded to decorator. 2 args username and message
+    return "{0}: {1}".format(username, message)
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)             #get IP and port
     

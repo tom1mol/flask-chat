130d1e46930e3a9282c4f4914fec157c7decabc3
tom1mol <tomasomaolmhuaidh@gmail.com>
2018-12-20 22:42:01 +0000

    storing our messages

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import os     #access environment variables
 from flask import Flask
 
-app = Flask(__name__)       #initialise new flask app
+app = Flask(__name__)       #initialise new flask app(define flask app)
+messages = []       #empty list
+
+def add_messages(username, message):        #function that will take username and message and append it to list
+    messages.append("{}: {}".format(username, message)) #append string using format method
 
 @app.route('/')     #root decorator for index page
 def index():            #define function that'll be bound to our decorator
@@ -12,11 +16,13 @@ def index():            #define function that'll be bound to our decorator
     
 @app.route('/<username>')       #this gets treated as a variable(username)
 def user(username):             #a function that will bind to the route decorator. argument of username
-    return "Hi " + username          #return to the user hi + username
+    """display chat messages"""
+    return "Welcome, {0}".format(username, messages)          #return to the user hi + username
     
     
 @app.route('/<username>/<message>')
 def send_message(username, message):     #function that is binded to decorator. 2 args username and message
+    """create a new message and redirect back to the chat page """
     return "{0}: {1}".format(username, message)
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)             #get IP and port

6389cdbd66d4c79fd148287c68a98760e91b4cd3
tom1mol <tomasomaolmhuaidh@gmail.com>
2019-01-08 14:54:34 +0000

    storing our messages in dict

--- run.py
+++ run.py
@@ -16,13 +16,16 @@ def add_messages(username, message):        #function that will take username an
     """Add messages to the 'messages' list"""
     now = datetime.now().strftime("%H:%M:%S") #strf method takes date/time obj and converts to a string using given format
                 # .now() method used to get current time
-    messages.append("({}) {}: {}".format(now, username, message)) #append string using format method
-    
-    
-def get_all_messages():
-    """Get all messages and seperate them with a 'br' """
-    return "<br>".join(messages)        #join method
-    
+                
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+            #dictionary to store message info. store as key value pairs.
+            #key= timestamp. value=now(was created on line above)
+            
+    #messages.append("({}) {}: {}".format(now, username, message)) #append string using format method
+    messages.append(messages_dict)
+
+ 
+
     
 @app.route('/', methods = ["GET", "POST"])  #root decorator for index page. get and post relates to form in index.html
 def index():            #define function that'll be bound to our decorator
@@ -44,7 +47,7 @@ def index():            #define function that'll be bound to our decorator
 def user(username):             #a function that will bind to the route decorator. argument of username
     """display chat messages"""
     #return "Welcome, {0}: {1}".format(username, messages )          #return to the user hi + username
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
+    return "<h1>Welcome, {0}</h1>{1}".format(username, messages)
     #welcome message in h1 tags..subsequent message(get-all-messages outside the h1)
 @app.route('/<username>/<message>')
 def send_message(username, message):     #function that is binded to decorator. 2 args username and message
